8a53f4066334caa4cc7616dbdc9be1c118dada7b
HyunKyu <HyunKyu@LIM_laptop>
2021-07-20 08:34:04 +0900

    seoulite80

--- src/com/sapyoung/seoulite80/MainTest.java
+++ src/com/sapyoung/seoulite80/MainTest.java
@@ -2,5 +2,16 @@ package com.sapyoung.seoulite80;
 
 public class MainTest {
 	int a1 = 3;
-	String a2 ="value";
+	char c1 = 'c';
+	float f1 = 3.14;
+	boolean b1 = false;
+	
+	Integer i1 = 3;
+	Integer i2 = 4;
+	String a2 = "value";
+	
+	public void disp() {
+		System.out.println("Hello World!");
+		
+	}
 }
