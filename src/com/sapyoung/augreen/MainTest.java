cba201f40fbff6bdc5c4cd14e0a97016edfa6b2c
user <user@HAYOUNG>
2021-07-20 09:56:18 +0900

    augreen 20210720

--- src/com/sapyoung/augreen/MainTest.java
+++ src/com/sapyoung/augreen/MainTest.java
@@ -1,5 +1,6 @@
 package com.sapyoung.augreen;
 
+import com.sapyoung.Gerbugi.day20210720.Gerbugi;
 import com.sapyoung.augreen.day20210720.Augreen;
 
 public class MainTest {
@@ -7,13 +8,17 @@ public class MainTest {
 	String s = "d";
 	
 	public static void main(String[] arg) {
-		Augreen w = new Augreen();
-		w.getMail();
-		w.getCompName();
-		w.getDepartName();
+		Augreen a = new Augreen();
+		a.getName();
+		a.getMail();
+		a.getCompName();
+		a.getDepartName();
 	
-	
-		
+		Gerbugi g = new Gerbugi();
+		g.getName();
+		g.getCompName();
+		g.getCompTel();
+		g.getId();
 	}
 	
 	
