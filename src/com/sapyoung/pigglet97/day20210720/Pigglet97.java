ae944412d893d736dfb38786158f9bab49d48ed2
SHO <SHO@DESKTOP-FBMNDKB>
2021-07-21 08:34:34 +0900

    pigglet97_0721

---
+++ src/com/sapyoung/pigglet97/day20210720/Pigglet97.java
@@ -0,0 +1,39 @@
+package com.sapyoung.pigglet97.day20210720;
+
+public class Pigglet97 {
+	
+	public void getName() {
+		System.out.println("이승은");	
+		}
+	
+	public void getDepartment() {
+		System.out.println("Bricks R&D");	
+	}
+	
+	public void getPosition() {
+		System.out.println("부장");	
+	}
+	
+	public void getMail() {
+		System.out.println("pigglet97@sapyoung.com");	
+	}
+	
+	public void desc() {
+	      System.out.println("교재개발을 담당하고 있습니다.");
+	   }
+	   
+	public void getId() {
+	      System.out.println(getClass().getSimpleName());
+	   }
+
+	public void getCompName() {
+	      System.out.println("사회평론");
+	   }
+	   
+	public void getCompTel() {
+	      System.out.println("02-326-1182");
+	   }
+	public void getCompSite() {
+		      System.out.println("https://www.sapyoung.com");
+		   }
+}
