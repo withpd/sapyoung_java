228b4aa48779b52add7379c4e975c24595d9a1b0
USER <USER@DESKTOP-GMDH2V2>
2021-07-21 08:33:45 +0900

    seravision3

--- src/com/sapyoung/seravision3/MainTest.java
+++ src/com/sapyoung/seravision3/MainTest.java
@@ -1,5 +1,6 @@
 package com.sapyoung.seravision3;
 
+import com.sapyoung.Gerbugi.day20210720.Gerbugi;
 import com.sapyoung.seravision3.day20210720.Seravision3;
 
 public class MainTest {
@@ -7,13 +8,18 @@ public class MainTest {
     String s ="d";
     		
 	public static void main(String[] arg) {
-		Seravision3 w = new Seravision3();
-		 
-		w.getMail();
-		w.getCompName();
-		w.getDepartName();
-		  
+		/*
+		 * Seravision3 w = new Seravision3();
+		 * 
+		 * w.getMail(); w.getCompName(); w.getDepartName();
+		 */
+		Gerbugi g = new Gerbugi();
+		g.getMail();
+		g.getCompName();
+		g.getDepartName();
+		g.getId();
 	  }
+	
 	
 }
 
