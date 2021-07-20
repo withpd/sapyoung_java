8056f6c9a7f49c4f627f821e1a226a78aafb6bb7
user <user@HAYOUNG>
2021-07-20 09:35:11 +0900

    augreen 20210720

--- src/com/sapyoung/augreen/MainTest.java
+++ src/com/sapyoung/augreen/MainTest.java
@@ -1,7 +1,26 @@
 package com.sapyoung.augreen;
 
+import com.sapyoung.augreen.day20210720.Augreen;
+
 public class MainTest {
-	int a1 = 3;
-	String a2 = "value";
+	
+	String s = "d";
+	
+	public static void main(String[] arg) {
+		Augreen w = new Augreen();
+		w.getMail();
+		w.getCompName();
+		w.getDepartName();
+	
+	
+		
+	}
+	
+	
+	
+	
+	
+	
+	
 	
 }
