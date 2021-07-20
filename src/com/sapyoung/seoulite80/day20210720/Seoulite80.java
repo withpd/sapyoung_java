905b8a00cc38ecddbe10e38e374c6a1087429d27
HyunKyu <HyunKyu@LIM_laptop>
2021-07-20 09:40:34 +0900

    Seoulite80

---
+++ src/com/sapyoung/seoulite80/day20210720/Seoulite80.java
@@ -0,0 +1,41 @@
+package com.sapyoung.seoulite80.day20210720;
+
+public class Seoulite80 {
+
+	public void getName() {
+		System.out.println("임현규");
+	}
+
+	public void getDepartName() {
+		System.out.println("아카데미");
+	}
+
+	public void getPosition() {
+		System.out.println("과장");
+	}
+
+	public void getMail() {
+		System.out.println("hklim@sapyoung.com");
+	}
+
+	public void desc() {
+		System.out.println("신사업팀을 맡고 있습니다.");
+	}
+
+	public void getId() {
+		System.out.println(getClass().getSimpleName());
+	}
+
+	public void getCompName() {
+		System.out.println("사회평론");
+	}
+
+	public void getCompTel() {
+		System.out.println("02-326-1182");
+	}
+
+	public void getCompSite() {
+		System.out.println("https://www.sapyoung.com");
+	}
+
+}
