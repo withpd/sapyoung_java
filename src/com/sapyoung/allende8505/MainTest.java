94cad595ebd3ed9c4841007ae73be809d58afbc8
allen <allen@DESKTOP-9215JLH>
2021-07-21 08:26:25 +0900

    20210721

--- src/com/sapyoung/allende8505/MainTest.java
+++ src/com/sapyoung/allende8505/MainTest.java
@@ -1,10 +1,23 @@
 package com.sapyoung.allende8505;
 
+import com.sapyoung.Gerbugi.day20210720.Gerbugi;
+import com.sapyoung.allende8505.day20210720.Allende8505;
+
 public class MainTest {
-	int a1 = 3;
-	String a2 = "value";
 	
-	public void disp() {
+	public static void main(String[] arg) {
+		Allende8505 alle = new Allende8505();
+		alle.getName();
+		alle.getposition();
+		alle.getCompName();
+		alle.getDepartName();
+		alle.getId();
 		
+		Gerbugi g = new Gerbugi();
+		g.getName();
+		g.getPosition();
+		g.getCompName();
+		g.getDepartName();
+		g.getId();
 	}
 }
