2f1ea0e244f6afc0aa61126e1ebfd1dc1bc7ab15
SPnt06 <SPnt06@DESKTOP-OUR6RV5>
2021-07-21 08:03:45 +0900

    add withpd package

---
+++ src/com/sapyoung/withpd/MainTest.java
@@ -0,0 +1,17 @@
+package com.sapyoung.withpd;
+
+import com.sapyoung.Gerbugi.day20210720.Gerbugi;
+
+public class MainTest {
+	public static void main(String[] args) {
+//		Withpd w = new Withpd();
+//		w.getCompName();
+//		w.getName();
+//		w.getId();
+		
+		Gerbugi g = new Gerbugi();
+		g.getCompName();
+		g.getName();
+		g.getId();
+	}
+}
