8fdf0abb38520a4220f9c6c91fa7f5e5b250b3d6
tiger <tiger@DESKTOP-EQ1L9CU>
2021-07-21 08:34:49 +0900

    # WARNING: head commit changed in the meantime
    las

---
+++ src/com/sapyoung/clara/MainTest.java
@@ -0,0 +1,23 @@
+package com.sapyoung.clara;
+
+import com.sapyoung.Gerbugi.day20210720.Gerbugi;
+
+import com.sapyoung.clara.leeaesook.day20210720.Clara;
+
+public class MainTest {
+	String str = new String("str");
+	
+	public static void main(String[] arg) {
+//		Clara c = new Clara();
+//		c.getMail();
+//		c.getComeName();
+//		c.getDepartName();
+		
+		Gerbugi g = new Gerbugi();
+		g.getCompName();
+		g.desc();
+		g.getCompTel();
+		g.getId();
+		
+	}
+}
---
+++ src/com/sapyoung/clara/leeaesook/day20210720/Clara.java
@@ -0,0 +1,32 @@
+package com.sapyoung.clara.leeaesook.day20210720;
+
+public class Clara {
+
+	public void getName( ) {
+		System.out.println("clara");
+	}
+	public void getDepartName() {
+		System.out.println("bricks");
+	}
+	public void getPosition( ) {
+		System.out.println("president");
+	}
+	public void getMail( ) {
+		System.out.println("las@ebricks.co.kr");
+	}
+	public void desc( ) {
+		System.out.println("R&D");
+	}
+	public void getId( ) {
+		System.out.println(getClass().getSimpleName());
+	}
+	public void getComeName( ) {
+		System.out.println("사회평론");
+	}
+	public void getComTel( ) {
+		System.out.println("02-326-1182");
+	}
+	public void geComSite( ) {
+		System.out.println("http://www.sapyoung.com");
+	}
+}
