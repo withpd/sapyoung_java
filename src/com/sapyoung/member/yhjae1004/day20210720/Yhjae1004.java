8e50408d0f9ad4c204ec08a3fecae5466a0430b7
1004y <1004y@DESKTOP-PKP3S66>
2021-07-22 08:51:38 +0900

    0722

---
+++ src/com/sapyoung/member/yhjae1004/day20210720/Yhjae1004.java
@@ -0,0 +1,40 @@
+package com.sapyoung.member.yhjae1004.day20210720;
+
+public class Yhjae1004 {
+	
+	public void getName() {
+		System.out.println("유희재");
+	}
+	
+	public void getDepartName() {
+		System.out.println("브릭스알앤디");
+	}
+	
+	public void getPosition() {
+		System.out.println("사원");
+	}
+	
+	public void getMail() {
+		System.out.println("yhjae92@ebricks.co.kr");
+	}
+	
+	public void desc() {
+		System.out.println("영어 콘텐츠 개발을 담당하고 있습니다.");
+	}
+	
+	public void getId() {
+		System.out.println(getClass().getSimpleName());
+	}
+	
+	public void getCompName() {
+		System.out.println("사회평론");
+	}
+	
+	public void getCompTel() {
+		System.out.println("02-326-1182");		
+	}
+	
+	public void getCompSite() {
+		System.out.println("https://www.sapyoung.com");
+	}
+}
---
+++ src/com/sapyoung/member/yhjae1004/day20210721/Yhjae1004.java
@@ -0,0 +1,48 @@
+package com.sapyoung.member.yhjae1004.day20210721;
+
+/*import com.sapyoung.company.Company;*/
+/*import com.sapyoung.ysb0807.*;*/
+public class Yhjae1004 {
+	
+	public String getName() {
+		String name = "유희재";
+		return name;
+	}
+	
+	public void getDepartName() {
+		System.out.println("브릭스알앤디");
+	}
+	
+	public String getPosition() {
+		String pos = "사원";
+		return pos;
+	}
+	
+	public void getMail() {
+		System.out.println("yhjae92@ebricks.co.kr");
+	}
+	
+	public void desc() {
+		System.out.println("영어 콘텐츠 개발을 담당하고 있습니다.");
+	}
+	
+	public void getId() {
+		System.out.println(getClass().getSimpleName());
+	}
+	
+	public static void main(String[] args) {
+		Yhjae1004 y = new Yhjae1004();
+		String name = y.getName();
+		String pos = y.getPosition();
+		
+		System.out.println("[" + pos + " " + name +"]");
+		y.desc();
+		
+		/*
+		 * Company c = new Company(); c.getCompSite();
+		 * 
+		 * Ysb0807 s = new Ysb0807(); s.getDepartName();
+		 */
+				
+	}
+}
