632b5c204a9c98c43e57fdc344b4e7e9b19223e8
82108 <82108@DESKTOP-HQ7AN0R>
2021-07-22 08:49:05 +0900

    member up

---
+++ src/com/sapyoung/member/ecodooly8/MainTest.java
@@ -0,0 +1,25 @@
+package com.sapyoung.member.ecodooly8;
+
+import com.sapyoung.Gerbugi.day20210720.Gerbugi;
+import com.sapyoung.ecodooly8.day20210719.Ecodooly8;
+
+public class MainTest {
+	String str = new String("str");
+	
+	String s = "d";
+	
+	public static void main(String [] arg) {
+		Ecodooly8 w = new Ecodooly8();
+		w.getMail();
+		w.getCompName();
+		w.getDepartName();
+	
+		Gerbugi g = new Gerbugi();
+		g.getCompName();
+		g.getName();
+		g.getId();
+	}
+	
+}
+
+
