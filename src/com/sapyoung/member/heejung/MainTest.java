1eb525fac049bcc443e88147a89eca7fb0531c05
user <user@DESKTOP-SB5OCUE>
2021-07-22 08:52:07 +0900

    hh

---
+++ src/com/sapyoung/member/heejung/MainTest.java
@@ -0,0 +1,21 @@
+package com.sapyoung.member.heejung;
+
+import com.sapyoung.Gerbugi.day20210720.Gerbugi;
+import com.sapyoung.heejung.day20210720.Heejung;
+
+public class MainTest {
+	
+	String s = "d";
+	
+	public static void main(String[] arg) {
+		Heejung h = new Heejung();
+		h.getMail();
+		h.getCompName();
+		h.getDepartName();
+		
+		
+		Gerbugi g = new Gerbugi();
+		g.getName();
+		g.getId();
+	}
+}
---
+++ src/com/sapyoung/member/heejung/day20210721/Heejung.java
@@ -0,0 +1,53 @@
+package com.sapyoung.member.heejung.day20210721;
+
+import com.sapyoung.Gerbugi.day20210720.Gerbugi;
+import com.sapyoung.company.Company;
+
+public class Heejung {
+	
+	public String getName() {
+		String name = "장희정";
+		return name;
+	}
+	
+	public void getFloor() {
+		System.out.println("3층");
+	}
+	
+	public void getDepartName() {
+		System.out.println("브릭스 R&D");
+	}
+	
+	public String getPosition() {
+		String pos = "차장";
+		return pos;
+	}
+	
+	public void getMail() {
+		System.out.println("eena@sapyoung.com");
+	}
+	
+	public void desc() {
+		System.out.println("교재 개발을 맡고 있습니다.");
+	}
+	
+	public void getId() {
+		System.out.println(getClass().getSimpleName());
+	}
+	
+	public static void main(String[] args) {
+		Heejung h = new Heejung();
+		String name = h.getName();
+		String pos = h.getPosition();
+		
+		System.out.println("[" + pos + " " + name + "]");
+		h.desc();
+		
+		Company c = new Company();
+		c.getCompSite();
+		
+		Gerbugi g = new Gerbugi();
+		g.getDepartName();
+		
+	}
+}
