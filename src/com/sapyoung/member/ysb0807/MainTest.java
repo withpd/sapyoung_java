172207c579d4b2d18f18a7d1f55136e9f7a2d1a5
Administrator <Administrator@DESKTOP-52BTSH6>
2021-07-22 08:52:37 +0900

    55

---
+++ src/com/sapyoung/member/ysb0807/MainTest.java
@@ -0,0 +1,19 @@
+package com.sapyoung.member.ysb0807;
+
+import com.sapyoung.Gerbugi.day20210720.Gerbugi;
+import com.sapyoung.ysb0807.day20210720.Ysb0807;
+
+public class MainTest {
+		
+	public static void main(String[] arg) {
+		/*
+		 * Ysb0807 ysb0807 = new Ysb0807(); ysb0807.getMail(); ysb0807.getCompName();
+		 * ysb0807.getDepartName();
+		 */
+		
+		Gerbugi g = new Gerbugi();
+		g.getCompSite();
+		g.getCompTel();
+		g.getId();
+	}
+}
