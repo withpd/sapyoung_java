3c0f3b0900ef7051c224f4de2968cee826347e06
dagki <dagki@LAPTOP-KGM0BO9L>
2021-07-22 08:44:23 +0900

    20217021

---
+++ src/com/sapyoung/member/histoyw/MainTest.java
@@ -0,0 +1,17 @@
+package com.sapyoung.member.histoyw;
+
+import com.sapyoung.Gerbugi.day20210720.Gerbugi;
+import com.sapyoung.histoyw.day20210720.Histoyw;
+
+public class MainTest {
+
+	public static void main(String[] arg) {
+		Gerbugi g = new Gerbugi();
+		g.getDepartName();
+		g.getName();
+		g.getCompName();
+	}
+	
+
+
+}
