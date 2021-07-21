9d42ec2ae3412225f3f116f45f169a3a3650c6b8
tiger <tiger@DESKTOP-EQ1L9CU>
2021-07-21 21:40:37 +0900

    t

---
+++ src/com/sapyoung/clara/day20210720/Clara.java
@@ -0,0 +1,44 @@
+package com.sapyoung.clara.day20210720;
+
+public class Clara {
+
+	public void getName() {
+		System.out.println("ÀÌ¾Ö¼÷");
+	}
+	
+	public void getPosition() {
+		System.out.println("´ëÇ¥");
+	}
+	
+	public void getDepartName() {
+		System.out.println("bricks");
+	}
+	
+	public void getFloor() {
+		System.out.println("3Ãþ");
+	}
+	
+	public void getMail() {
+		System.out.println("las@ebricks.co.kr");
+	}
+	
+	public void desc() {
+		System.out.println("R&D");
+	}
+	
+	public void getId() {
+		System.out.println(getClass().getSimpleName());
+	}
+	
+	public void getComeName( ) {
+		System.out.println("»çÈ¸Æò·Ð");
+	}
+	
+	public void getComTel( ) {
+		System.out.println("02-326-1182");
+	}
+	
+	public void geComSite( ) {
+		System.out.println("http://www.sapyoung.com");
+	}
+}
