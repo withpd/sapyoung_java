a4ab07d38dacf866d2dbd6fd8684fda90f1ade77
SPnt10 <SPnt10@DESKTOP-P7D7VB8>
2021-07-22 09:17:50 +0900

    gg

--- src/com/sapyoung/member/Gerbugi/day20210721/Gerbugi.java
+++ src/com/sapyoung/member/Gerbugi/day20210721/Gerbugi.java
@@ -1,7 +1,7 @@
 package com.sapyoung.member.Gerbugi.day20210721;
 
 import com.sapyoung.company.Company;
-import com.sapyoung.hooni7777.day20210720.Hooni7777;
+import com.sapyoung.member.hooni7777.day20210720.Hooni7777;
 
 public class Gerbugi {
 	
