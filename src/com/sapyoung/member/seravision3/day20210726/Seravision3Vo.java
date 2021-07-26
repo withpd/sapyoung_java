99ae24a9c85a88281bb51e22b263c1e8b0d16298
USER <USER@DESKTOP-GMDH2V2>
2021-07-26 13:04:46 +0900

    seravision3

---
+++ src/com/sapyoung/member/seravision3/day20210726/Seravision3Vo.java
@@ -0,0 +1,51 @@
+package com.sapyoung.member.seravision3.day20210726;
+
+public class Seravision3Vo {
+    
+	private String id ; 
+	private String mail ; 
+	private String floor ; 
+	private String name ; 
+	private String department ; 
+	private String pos ;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getFloor() {
+		return floor;
+	}
+	public void setFloor(String floor) {
+		this.floor = floor;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public String getPos() {
+		return pos;
+	}
+	public void setPos(String pos) {
+		this.pos = pos;
+	} 
+	
+		
+	
+	
+}
