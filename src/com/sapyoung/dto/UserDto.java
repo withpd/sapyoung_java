0ff1dd7357f88ef1d37a86c140cb3c6290d72c9c
withpd <withpd@gmail.com>
2021-07-27 09:54:20 +0900

    UserDto

--- src/com/sapyoung/dto/UserDto.java
+++ src/com/sapyoung/dto/UserDto.java
@@ -8,6 +8,25 @@ public class UserDto {
 	private String departName;
 	private String pos;
 	
+	public UserDto() {}
+	
+	public UserDto(int floor) {
+		
+		setFloor(floor);
+		
+		if(floor == 2) setDepartName("어린이사업부");
+		else if(floor == 3) setDepartName("개발팀");
+		else if(floor == 4) setDepartName("아카데미");
+		else setDepartName("경영");
+		
+		/*
+		 *  2층 : 어린이사업부
+		 *  3층 : 개발팀
+		 *  4층 : 아카데미
+		 *  5층 : 경영
+		 */
+	}
+	
 	public String getId() {
 		return id;
 	}
@@ -32,6 +51,19 @@ public class UserDto {
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	public void setName() {
+		System.out.println("");
+	}
+	
+	public void setName(int num) {
+		System.out.println("");
+	}
+	
+	public void setName(int num, String str) {
+		System.out.println("");
+	}
+	
 	public String getDepartName() {
 		return departName;
 	}
