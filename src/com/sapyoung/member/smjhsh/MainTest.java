7295212e857db4f300d13c7b945be35e233f2f34
toffl <toffl@DESKTOP-SH>
2021-07-28 09:52:47 +0900

    0728

--- src/com/sapyoung/member/smjhsh/MainTest.java
+++ src/com/sapyoung/member/smjhsh/MainTest.java
@@ -1,15 +1,30 @@
 package com.sapyoung.member.smjhsh;
 
-import com.sapyoung.member.smjhsh.day20210720.Smjhsh;
+import com.sapyoung.member.smjhsh.day20210722.Smjhsh;
 
-public class MainTest {
-	String str =new String("str");
-	
-	public static void main(String[] arg) {
-		Smjhsh w = new Smjhsh();	
-		w.getMail();
-		w.getCompName();
-		w.getDepartName();
-		}	
-
+public class MainTest {	
+	public static void main(String[] args) {
+//			Smjhsh w = new Smjhsh();
+//			String name = w.getName();          //return À¯
+//			String pos = w.getPosition();  
+//			w.getFloor();
+//			
+			
+//			System.out.println("[" + pos + " " +name+"]");
+//			w.desc();
+//			
+//			Company c = new Company();
+//			c.getCompSite();
+//			
+//			Peepstest p = new Peepstest();
+//			p.getDepartName();
+			
+			Smjhsh smjhsh = new Smjhsh();
+			smjhsh.desc();
+			String id = smjhsh.getId();
+			
+			id = "[" + id + "]" + 1111;
+			
+			System.out.println(id);
+	}
 }
---
+++ src/com/sapyoung/member/smjhsh/day20210728/Main.java
@@ -0,0 +1,15 @@
+package com.sapyoung.member.smjhsh.day20210728;
+
+import com.sapyoung.company.Company;
+import com.sapyoung.dto.UserDto;
+
+public class Main extends Company{
+	public static void main(String[] args) {
+		Main m = new Main();
+		m.getCompName();
+		String comName = m.getCompName();
+		System.out.println(comName);
+		
+		
+	}
+}
