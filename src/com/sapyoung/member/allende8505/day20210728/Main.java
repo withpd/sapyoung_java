70f15e6fb22d17ece3dc4b8c1c50caf07e755409
allen <allen@DESKTOP-9215JLH>
2021-07-29 08:26:45 +0900

    20210729

---
+++ src/com/sapyoung/member/allende8505/day20210728/Main.java
@@ -0,0 +1,47 @@
+package com.sapyoung.member.allende8505.day20210728;
+
+import com.sapyoung.company.Company;
+//import com.sapyoung.dto.UserDto;
+
+public class Main extends Company{
+		public static void main(String[] args) {
+			Main m = new Main();
+			String comName = m.getCompName();
+			System.out.println(comName);
+		
+		
+		
+		
+		
+		
+		
+		
+//		기본생성자 사용
+//		UserDto dto = new UserDto();
+//		
+//		dto.setName("박동명");
+//		dto.setDepartName("사평마케팅");
+//		dto.setFloor(2);
+//		dto.setId("allende8505");
+//		dto.setPos("대리");
+//		dto.setMail("allende8505@sapyoung.com");
+//		
+//		일반생성자 #1
+//		UserDto dto2 = new UserDto(2);
+//		
+//		dto2.setName("박동명");
+//		dto2.setId("allende8505");
+//		dto2.setPos("대리");
+//		dto2.setMail("allende8505@sapyoung.com");
+//
+//		일반생성자 #2
+//		UserDto dto3 = new UserDto(2,"사평마케팅");
+//		
+//		dto2.setName("박동명");
+//		dto2.setId("allende8505");
+//		dto2.setPos("대리");
+//		dto2.setMail("allende8505@sapyoung.com");
+		
+
+	}
+}
