b6b672b6b2f446e26ca083cff90f5af1aa2d64e7
Administrator <Administrator@DESKTOP-52BTSH6>
2021-07-28 09:55:35 +0900

    ysb

---
+++ src/com/sapyoung/member/ysb0807/day20210728/Main.java
@@ -0,0 +1,13 @@
+package com.sapyoung.member.ysb0807.day20210728;
+
+import com.sapyoung.company.Company;
+
+public class Main extends Company{
+	public static void main(String[] args) {
+		Main m = new Main();
+		String comName = m.getCompName();
+		System.out.println(comName);
+			
+		
+	}
+}
