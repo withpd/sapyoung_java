8c0a3913960e1e852107a2e0a44026e26e467c50
SHO <SHO@DESKTOP-FBMNDKB>
2021-07-29 08:29:56 +0900

    pigglet

---
+++ src/com/sapyoung/member/pigglet97/day20210727/Main.java
@@ -0,0 +1,24 @@
+package com.sapyoung.member.pigglet97.day20210727;
+
+import com.sapyoung.dto.UserDto;
+
+public class Main {
+	public static void main (String[] args) {
+		
+		
+	/*	UserDto dto = new UserDto(); //메소드 중에 클래스이름일 경우 생성자라 함, 생성자 중에 파라미터 없는 것(기본생성자)은 생략이 가능, 기본 생성자는 리턴타입이 없다. 기본 생성자가 아닌 생성자를 쓰려면 생략불가
+		dto.setName("이승은");
+		dto.setDepartName("Bricks");
+		dto.setFloor(3);
+		dto.setId("pigglet97");
+		dto.setPos("부장");
+		dto.setMail("pigglet97@sapyoug.com");
+		
+		String name = dto.getName();
+		String pos = dto.getPos();
+		
+		String result = pos + " " + name;
+		System.out.println(result);
+		System.out.println(dto.getMail());*/
+	}
+}
