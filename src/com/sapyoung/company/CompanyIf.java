584381b2262398ebb5572898781f3241119f140c
withpd <withpd@gmail.com>
2021-07-29 10:07:38 +0900

    interface

---
+++ src/com/sapyoung/company/CompanyIf.java
@@ -0,0 +1,10 @@
+package com.sapyoung.company;
+
+public interface CompanyIf {
+	public void setCompName();
+	public void getCompName();
+	public void setMail();
+	public void getMail();
+	public void setPos();
+	public void getPos();
+}
