b47011ec8bf584aa91597c6a495ad5021cb911e9
allen <allen@DESKTOP-9215JLH>
2021-08-02 08:26:54 +0900

    20210802

---
+++ src/com/sapyoung/member/allende8505/day20210729/Allende8505.java
@@ -0,0 +1,43 @@
+package com.sapyoung.member.allende8505.day20210729;
+
+import com.sapyoung.company.CompanyIf;
+
+public class Allende8505 implements CompanyIf{
+
+	@Override
+	public void setCompName() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getCompName() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setMail() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getMail() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setPos() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getPos() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
