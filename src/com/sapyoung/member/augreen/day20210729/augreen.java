317d404b9b8db339ec45b2bddb6da3cc43c636d2
user <user@HAYOUNG>
2021-08-02 08:25:22 +0900

    # WARNING: head commit changed in the meantime
    augreen 28

---
+++ src/com/sapyoung/member/augreen/day20210729/augreen.java
@@ -0,0 +1,39 @@
+package com.sapyoung.member.augreen.day20210729;
+
+import com.sapyoung.company.CompanyIf;
+
+public class augreen implements CompanyIf {
+
+	@Override
+	public void setCompName() {
+		
+	}
+
+	@Override
+	public void getCompName() {
+		
+	}
+
+	@Override
+	public void setMail() {
+		
+	}
+
+	@Override
+	public void getMail() {
+		
+	}
+
+	@Override
+	public void setPos() {
+		
+	}
+
+	@Override
+	public void getPos() {
+		
+	}
+
+
+	
+}
