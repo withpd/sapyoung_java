c1fb5e003dbe3d14829b2bead8b9bb58f5546386
USER <USER@DESKTOP-4QG4KA1>
2021-08-02 10:21:01 +0900

    0802 imachiri

--- src/com/sapyoung/member/imachiri/MainTest.java
+++ src/com/sapyoung/member/imachiri/MainTest.java
@@ -1,33 +1,16 @@
+//구조: WorkIf를 WorkAb가 구현하고, WorkAb를 Work가 상속받음. Work를 MainTest에서 사용함.
+
 package com.sapyoung.member.imachiri;
 
-/*import com.sapyoung.member.Gerbugi.day20210720.*;
-import com.sapyoung.member.imachiri.day20210720.*;*/
-import com.sapyoung.member.imachiri.day20210722.Imachiri;
+import com.sapyoung.member.imachiri.day20210802.Work;
 
 public class MainTest {
 
 	public static void main(String[] arg) {
-	
-		/*
-		 * Imachiri i = new Imachiri(); i.getMail(); i.getPosition(); i.getId();
-		 * i.getDepartname();
-		 * 
-		 * String name = i.getName(); String pos = i.getPosition();
-		 * 
-		 * System.out.println(name + " " + pos);
-		 */	
-	
-		/*
-		 * Gerbugi g = new Gerbugi(); g.getCompName(); g.getCompTel(); g.getId();
-		 */
-		
-	Imachiri imachiri = new Imachiri();
-	imachiri.desc();
-	String id = imachiri.getId();
-	id = "[" + id + "]";
-	System.out.println(id);
-	
-		
+		Work w = new Work();
+		w.init("allende8505");
+		String result = w.getResult();
+		System.out.println(result);
 	}
 
 }
