c7b9faa68ef0dfd8b091592b11d3b192325e504b
USER <USER@DESKTOP-GMDH2V2>
2021-08-02 23:32:46 +0900

    seravision3

--- src/com/sapyoung/member/seravision3/MainTest.java
+++ src/com/sapyoung/member/seravision3/MainTest.java
@@ -1,37 +1,13 @@
 package com.sapyoung.member.seravision3;
 
-import com.sapyoung.member.Gerbugi.day20210720.Gerbugi;
-import com.sapyoung.member.seravision3.day20210722.Seravision3;
-import com.sapyoung.member.withpd.day20210726.Withpd;
+import com.sapyoung.member.seravision3.day20210802.Work;
 
 public class MainTest {
-	public static void main(String[] arg) {
-		/*
-		 * Seravision3 w = new Seravision3();
-		 * 
-		 * w.getMail(); w.getCompName(); w.getDepartName();
-		 */
-//		Gerbugi g = new Gerbugi();
-//		g.getMail();
-//		g.getCompName();
-//		g.getDepartName();
-//		g.getId();
-
-		Seravision3 seravision3 = new Seravision3();
-		Seravision3.desc();
-		String id = seravision3.getId();
-
-		
-		id = "[" + id + "]" + 1111;
-
-		System.out.println(id);
-
-		Seravision3 s = new Seravision3();
-		String pos = s.getPosition();
-		System.out.println(pos);
-		Withpd w = new Withpd();
-//		String pos2 = w.getPosition();
-//		System.out.println(pos2);
-
+	public static void main(String[] args) {
+		//work (WorkAb, WorkIf)
+		Work w = new Work();
+		w.init("seravision3");  
+		String result = w.getResult();
+		System.out.println(result);
 	}
 }
---
+++ src/com/sapyoung/member/seravision3/day20210802/WorkAb.java
@@ -0,0 +1,15 @@
+package com.sapyoung.member.seravision3.day20210802;
+
+public abstract class WorkAb implements WorkIf{
+	
+	public String getResult() {
+        String val = process();
+        return val;
+	}
+	
+}
+  
+	
+	
+	
+
