46df944f7d7ed963595f75af3ad70a7f52d9c3fb
withpd <withpd@gmail.com>
2021-08-04 09:51:34 +0900

    company

--- src/com/sapyoung/company/Company.java
+++ src/com/sapyoung/company/Company.java
@@ -4,17 +4,17 @@ public class Company {
 	private String comName;
 	
 	public Company() {
-		System.out.println("Company 기본 생성자");
+//		System.out.println("Company 기본 생성자");
 		comName = "A회사";
 	}
 	
 	public Company(String str) {
-		System.out.println("Company 생성자 #1");
+//		System.out.println("Company 생성자 #1");
 		comName = str;
 	}
 
 	public Company(int num) {
-		System.out.println("Company 생성자 #2");
+//		System.out.println("Company 생성자 #2");
 		comName = num + "회사";
 	}
 	
