28524da10e5fdf512ba07c3385bc4ebe194ece90
withpd <withpd@gmail.com>
2021-08-04 09:51:17 +0900

    for

---
+++ src/com/sapyoung/member/withpd/day20210804/DtoList.java
@@ -0,0 +1,78 @@
+package com.sapyoung.member.withpd.day20210804;
+
+import java.util.ArrayList;
+
+import com.sapyoung.dto.UserDto;
+
+public class DtoList {
+	public static void main(String[] args) {
+		ArrayList<UserDto> dtoList = new ArrayList<>();
+		
+		UserDto dto1 = new UserDto();
+		dto1.setDepartName("기타");
+		dto1.setName("배성호");
+		dto1.setMail("withpd@gmail.com");
+		dto1.setPos("강사");
+		
+		UserDto dto2 = new UserDto();
+		dto2.setDepartName("기타");
+		dto2.setName("강길호");
+		dto2.setMail("kangkilho@gmail.com");
+		dto2.setPos("없음");
+		
+		UserDto dto3 = new UserDto();
+		dto3.setDepartName("기타");
+		dto3.setName("김기령");
+		dto3.setMail("ddd@gmail.com");
+		dto3.setPos("없음");
+		
+		dtoList.add(dto1);
+		dtoList.add(dto2);
+		dtoList.add(dto3);
+		
+		int size = dtoList.size();
+//		System.out.println(size);
+		
+		boolean isContain = dtoList.contains(dto1);
+//		System.out.println(isContain);
+		
+		
+		
+		
+		
+		int size1 = dtoList.size();
+//		System.out.println(size1);
+//		
+		if(size1 != 0) {
+			UserDto d = dtoList.get(1);
+			String name = d.getName();
+//			System.out.println(name);
+		} else {
+//			System.out.println("데이터 없음");
+		}
+		
+		for(int i=0; i < dtoList.size(); i++) {
+			UserDto d = dtoList.get(i);
+			String name = d.getName();
+			System.out.println(name);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
---
+++ src/com/sapyoung/member/withpd/day20210804/StrList.java
@@ -0,0 +1,23 @@
+package com.sapyoung.member.withpd.day20210804;
+
+import java.util.ArrayList;
+
+public class StrList {
+	public static void main(String[] args) {
+		ArrayList<String> strList = new ArrayList<>();
+		
+		strList.add("배성호");
+		strList.add("강길호");
+		strList.add("김기령");
+		strList.add("이상덕");
+		
+		int size = strList.size();
+		System.out.println(size);
+		
+		boolean isContain = strList.contains("가나다");
+		System.out.println(isContain);
+		
+		String myName = strList.get(0);
+		System.out.println(myName);
+	}
+}
