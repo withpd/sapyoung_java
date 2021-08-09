ae5f72930a15068b2ac2a5e7202ded527a15e86b
suji2 <suji2@LAPTOP-FEMCQL1D>
2021-08-10 08:29:17 +0900

    f

---
+++ src/com/sapyoung/crawl/peepstest/Main.java
@@ -0,0 +1,33 @@
+package com.sapyoung.crawl.peepstest;
+
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		Document doc = Jsoup.connect("https://en.wikipedia.org/").get();
+		System.out.println(doc.title());
+		Elements newsHeadlines = doc.select("#mp-itn b a");
+		for (Element headline : newsHeadlines) {
+			System.out.println(headline.attr("title"));
+			System.out.println(headline.absUrl("href"));
+			
+
+		}
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+
+}
