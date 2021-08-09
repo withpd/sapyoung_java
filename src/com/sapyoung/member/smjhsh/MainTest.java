d4d4b4187a236320ec37c310236cbe43caa8ad92
toffl <toffl@DESKTOP-SH>
2021-08-09 10:07:39 +0900

    ddd

--- src/com/sapyoung/member/smjhsh/MainTest.java
+++ src/com/sapyoung/member/smjhsh/MainTest.java
@@ -3,12 +3,12 @@ package com.sapyoung.member.smjhsh;
 import com.sapyoung.member.smjhsh.day20210802.Work;
 
 public class MainTest {	
-	public static void main(String[] args) {
-		// Work (WorkAb,WorkIf)
-		Work w = new Work();
-		w.init("clara");
-		w.process();
-		String result = w.getResult();
-		System.out.println(result);
-	}
+//	public static void main(String[] args) {
+//		// Work (WorkAb,WorkIf)
+//		Work w = new Work();
+//		w.init("clara");
+//		w.process();
+//		String result = w.getResult();
+//		System.out.println(result);
+//	}
 }
