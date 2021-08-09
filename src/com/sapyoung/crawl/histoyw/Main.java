5a12c50f8284903af40e52e9dfce63214a954579
dagki <dagki@LAPTOP-KGM0BO9L>
2021-08-10 08:30:09 +0900

    0809

--- src/com/sapyoung/crawl/histoyw/Main.java
+++ src/com/sapyoung/crawl/histoyw/Main.java
@@ -7,6 +7,7 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+
 public class Main {
 	public static void main(String[] args) throws IOException {
 		Document doc = Jsoup.connect("https://en.wikipedia.org/").get();
--- src/com/sapyoung/member/histoyw/MainTest.java
+++ src/com/sapyoung/member/histoyw/MainTest.java
@@ -2,6 +2,7 @@ package com.sapyoung.member.histoyw;
 
 import com.sapyoung.member.histoyw.day20210802.Work;
 
+
 public class MainTest {
 
 	public static void main(String[] arg) {
