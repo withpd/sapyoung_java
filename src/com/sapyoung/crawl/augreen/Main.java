b52f9a76b5e4bd9c1cfcd82cbbfb2d050f0af241
user <user@HAYOUNG>
2021-08-11 08:24:21 +0900

    augreen

--- src/com/sapyoung/crawl/augreen/Main.java
+++ src/com/sapyoung/crawl/augreen/Main.java
@@ -9,15 +9,14 @@ import org.jsoup.select.Elements;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		Document doc = Jsoup.connect("https://en.wikipedia.org/").get();
-		System.out.println(doc);
-		System.out.println(doc.title());
-		Elements newsHeadlines = doc.select("#mp-itn b a");
-		for (Element headline : newsHeadlines) {
-			System.out.println(headline.attr("title"));
-			System.out.println(headline.absUrl("href"));					
-		}
+		StockMain stockMain = new StockMain();
+		stockMain.init("086900");
+		String result = stockMain.getResult();
+		System.out.println(result);
+	}	
+		
+}		
 	
-	}
-
-}
+		
+		
+		
