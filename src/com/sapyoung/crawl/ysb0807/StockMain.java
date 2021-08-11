8ad685cf29411edbf01810eac5d1e31adb7ff0a9
Administrator <Administrator@DESKTOP-52BTSH6>
2021-08-12 08:49:25 +0900

    ysb

---
+++ src/com/sapyoung/crawl/ysb0807/StockMain.java
@@ -0,0 +1,22 @@
+package com.sapyoung.crawl.ysb0807;
+
+import com.sapyoung.common.WorkAb;
+
+public class StockMain extends WorkAb{
+
+	@Override
+	public void init(String id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String process() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
