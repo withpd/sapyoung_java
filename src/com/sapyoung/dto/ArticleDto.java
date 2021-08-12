cb1aaee0456d397b5556dfaab76e970d61b2338e
withpd <withpd@gmail.com>
2021-08-12 09:36:31 +0900

    ArticleDto

---
+++ src/com/sapyoung/dto/ArticleDto.java
@@ -0,0 +1,36 @@
+package com.sapyoung.dto;
+
+public class ArticleDto {
+	
+	private String content = "";
+	private String url = "";
+	private String imgUrl = "";
+	private String title = "";
+	
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getImgUrl() {
+		return imgUrl;
+	}
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	
+}
