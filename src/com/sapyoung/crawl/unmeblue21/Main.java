bbeadd5ca0ffb17b2a1b0541778c1b69c50bf79c
Soojung Do <Soojung Do@DESKTOP-VLG2V0K>
2021-08-12 09:23:04 +0900

    0812

--- src/com/sapyoung/crawl/unmeblue21/Main.java
+++ src/com/sapyoung/crawl/unmeblue21/Main.java
@@ -1,8 +1,18 @@
 package com.sapyoung.crawl.unmeblue21;
 
-public class Main {
+import java.io.IOException;
 
-	
-	
-	
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		StockMain stockMain = new StockMain();
+		stockMain.init(null);
+		String result = stockMain.getResult();
+		System.out.println(result);
+		
+	}	
 }
---
+++ src/com/sapyoung/crawl/unmeblue21/StockMain.java
@@ -0,0 +1,57 @@
+package com.sapyoung.crawl.unmeblue21;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import com.sapyoung.common.WorkAb;
+
+public class StockMain extends WorkAb{
+
+	public static void main(String[] args) throws IOException {
+		
+		ArrayList<String> codeList = new ArrayList<>();
+		codeList.add("323410");
+		codeList.add("005930");
+		codeList.add("051910");
+		codeList.add("259960");
+		codeList.add("086900");
+		
+		while(true) {
+		
+			for(int i=0; i<codeList.size(); i++) {
+				String code = codeList.get(i);
+				String url = "https://finance.naver.com/item/main.nhn?code=" + code;
+				Document doc = Jsoup.connect(url).get();
+						
+			//종목명
+			Elements nameElements = doc.getElementsByAttributeValue("class", "wrap_company");
+			Elements h2Elements = nameElements.select("h2");
+			
+			System.out.println("종목명: " + h2Elements.text());
+			
+			//현재가
+			Elements e1 = doc.getElementsByAttributeValue("class", "no_today");
+			Element e2 = e1.get(0);
+			Elements e3 = e2.select("span");
+			
+			System.out.println("현재가: " + e3.get(0).text());
+			}
+		}
+	}
+	@Override
+	public void init(String id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String process() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
