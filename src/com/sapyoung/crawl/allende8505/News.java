c7a10a3442e36c24fea724d1d2a3a7d452c1a1c6
allen <allen@DESKTOP-9215JLH>
2021-08-13 10:00:32 +0900

    20210813

--- src/com/sapyoung/crawl/allende8505/News.java
+++ src/com/sapyoung/crawl/allende8505/News.java
@@ -1,23 +1,37 @@
 package com.sapyoung.crawl.allende8505;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import com.sapyoung.dto.ArticleDto;
+
 public class News {
+	
 	public static void main(String[] args) throws IOException {
+		
 		int page = 3;
 		
-		for(int j=1; j<page; j++) {
+		ArrayList<ArrayList<ArticleDto>> pageList = new ArrayList();
+		
+		for(int j=1; j<page; j++) {		//페이지 처리
+			
 			String url = "https://news.naver.com/main/list.naver?mode=LS2D&mid=shm&sid2=249&sid1=102&date=20210811&page=" + j;
 			Document doc = Jsoup.connect(url).get();
 			Elements elements = doc.getElementsByAttributeValue("class", "list_body newsflash_body");
 			Element element = elements.get(0);
 			Elements photoElements = element.getElementsByAttributeValue("class", "photo");
-			for(int i=0; i<photoElements.size(); i++) {
+			
+			ArrayList<ArticleDto> articleList = new ArrayList();
+			
+			for(int i=0; i<photoElements.size(); i++) {  //20건 기사 처리
+				
+				ArticleDto aDto = new ArticleDto();
+				
 				Element articleElement = photoElements.get(i);
 				Elements aElements = articleElement.select("a");
 				Element aElement = aElements.get(0);
@@ -30,14 +44,20 @@ public class News {
 				Element contentElement = subDoc.getElementById("articleBodyContents");
 				String content = contentElement.text();		//기사내용
 				
-				System.out.println(title);
-				System.out.println(content);
-				System.out.println();
+				aDto.setContent(content);
+				aDto.setContent(imgUrl);
+				aDto.setContent(title);
+				aDto.setContent(url);
+				
+				articleList.add(aDto);
+				System.out.println(articleList.size());
+				
 			}
-			System.out.println(j + "page 크롤링 종료");
-//		Elements aElements = element.select("a");
-//		Elements imgElements = element.select("img");
-//		System.out.println(aElements.size());
+			
+			pageList.add(articleList);
+			System.out.println("pagelist size" + pageList.size());	
+			System.out.println(j + "page 크롤링 종료");	
+			
 		}
 	}
 }
