edadc658c67fb029887329775ec32865b274ad7a
withpd <withpd@gmail.com>
2021-08-26 08:46:39 +0900

    lib

--- src/com/sapyoung/crawl/withpd/ExcelMain.java
+++ src/com/sapyoung/crawl/withpd/ExcelMain.java
@@ -2,11 +2,16 @@ package com.sapyoung.crawl.withpd;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -17,9 +22,6 @@ public class ExcelMain {
 		XSSFWorkbook workbook = new XSSFWorkbook(fis);
 		
 		XSSFSheet sheet = workbook.getSheetAt(0);
-//		Cell cell = sheet.getRow(3).getCell(1);
-//		System.out.println(cell);
-		
 		Iterator<Row> iter = sheet.rowIterator();
 		
 		while( iter.hasNext() ) {
@@ -30,18 +32,56 @@ public class ExcelMain {
 				System.out.println(c);
 			}
 		}
+		workbook.close();
+	}
+	
+	public void write(String path, String fileName) throws IOException {
+		Workbook workBook = new XSSFWorkbook();
+		
+		Sheet sheet0 = workBook.createSheet();
+		Sheet sheet1 = workBook.createSheet();
+		Sheet sheet2 = workBook.createSheet("sheetName");
+		
+		Row row0 = sheet0.createRow(0);
+		Row row1 = sheet0.createRow(1);
+		Row row2 = sheet0.createRow(2);
+		
+		row0.createCell(0).setCellValue("Name");
+		row0.createCell(1).setCellValue("Mail");
+		row0.createCell(2).setCellValue("Addr");
+		
+		row1.createCell(0).setCellValue("배성호");
+		row1.createCell(1).setCellValue("withpd@gmail.com");
+		row1.createCell(2).setCellValue("인천");
+		
+		row2.createCell(0).setCellValue("철수");
+		row2.createCell(1).setCellValue("cs@gmail.com");
+		row2.createCell(2).setCellValue("서울");
+		
+		FileOutputStream fos = new FileOutputStream(path + "\\" + fileName);
+		workBook.write(fos);
+		fos.close();
+		workBook.close();
 		
 	}
 	
 	public static void main(String[] args) {
 		String path = "C:\\Users\\SPnt06\\Desktop";
-		String fileName = "test.xlsx";
+		String fileName = "write.xlsx";
 		
 		ExcelMain em = new ExcelMain();
 		try {
-			em.read(path, fileName);
+			em.write(path, fileName);
 		} catch(IOException e) {
-			e.printStackTrace();
+			System.out.println("열려 있는 파일이 있습니다.");
 		}
 	}
+	
+	
+	
+	
+	
+	
+	
+	
 }
