b4c639b21a724533b513c2345bb2310fdbba1a52
Soojung Do <Soojung Do@DESKTOP-VLG2V0K>
2021-08-26 09:31:09 +0900

    20210826

---
+++ src/com/sapyoung/member/unmeblue21/day20210823/TxtMain.java
@@ -0,0 +1,60 @@
+package com.sapyoung.member.unmeblue21.day20210823;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class TxtMain {
+
+	public void write(String filePath, String fileName, String content) {
+		File file = new File(filePath, fileName);	
+		BufferedWriter bw;
+		
+		try {
+			FileWriter reader = new FileWriter(file);
+			bw = new BufferedWriter(reader);
+			
+			bw.write(content);
+			bw.close();
+		
+		} catch(Exception e) {
+		}		
+	}	
+		
+	public String read(String filePath, String fileName) {
+		File file = new File(filePath, fileName);		// 생성자
+		BufferedReader br;
+		
+		String result = "";
+		try {
+			FileReader reader = new FileReader(file);
+			br = new BufferedReader(reader);
+			
+//			System.out.println(br.readLine());
+			
+			String line = "";
+			
+			while( (line = br.readLine()) !=null ) {
+				result += line + "\n";				
+			}
+			
+		} catch(Exception e) {
+		} 
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		
+		TxtMain tm = new TxtMain();
+
+		// READ 예제
+//		String res = tm.read("C:\\Users\\Soojung Do\\Desktop", "test.txt");
+//		System.out.println(res);
+		
+		//WRITE 예제
+		tm.write("C:\\Users\\Soojung Do\\Desktop", "test2.txt", "안녕하세요!\n테스트2번 파일 입니다.");
+					
+	}
+}
---
+++ src/com/sapyoung/member/unmeblue21/day20210824/ExcelMain.java
@@ -0,0 +1,87 @@
+package com.sapyoung.member.unmeblue21.day20210824;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelMain {
+
+	public void read(String path, String filename) throws IOException {
+		
+		FileInputStream fis = new FileInputStream(path + "\\" + filename);
+		XSSFWorkbook workbook = new XSSFWorkbook(fis);
+			
+		XSSFSheet sheet = workbook.getSheetAt(0);			//	-> getSheetAt은 시트 순서로 가져오는 것
+//		XSSFSheet sheet = workbook.getSheet("Sales Tax");	//	-> getSheet는 시트명으로 가져오는 것
+		
+//		Cell cell = sheet.getRow(3).getCell(1);  			//	-> 4행 2열(B열) 데이터 가져옴
+//		System.out.println(cell);
+
+		Iterator<Row> iter = sheet.rowIterator();
+		
+		while( iter.hasNext() ) {							//	while은 조건이 참이면 무조건 작동
+			Row r = iter.next();
+															//	for는 조건에 따른 숫자값이 맞으면 작동
+			for(int i=0; i < r.getPhysicalNumberOfCells(); i++) {
+				Cell c = r.getCell(i);
+				System.out.println(c);
+			}
+		}
+		workbook.close();
+	}
+
+	public void write(String path, String filename) throws IOException {
+		Workbook workbook = new XSSFWorkbook();
+		
+		Sheet sheet0 = workbook.createSheet();				//	시트1번, 네이밍 자동
+		Sheet sheet1 = workbook.createSheet();				//	시트2번, 네이밍 자동
+		Sheet sheet2 = workbook.createSheet("시트이름");		//	시트3번, 지정 네이밍
+		
+		Row row0 = sheet0.createRow(0);
+		Row row1 = sheet0.createRow(1);
+		Row row2 = sheet0.createRow(2);
+
+		row0.createCell(0).setCellValue("NAME");
+		row0.createCell(1).setCellValue("MAIL");
+		row0.createCell(2).setCellValue("Addr");
+
+		row1.createCell(0).setCellValue("최일규");
+		row1.createCell(1).setCellValue("with@sapyoung.com");
+		row1.createCell(2).setCellValue("부천");
+		
+		row2.createCell(0).setCellValue("장동건");
+		row2.createCell(1).setCellValue("jangdg@sapyoung.com");
+		row2.createCell(2).setCellValue("안드로메다");		
+		
+		FileOutputStream fos = new FileOutputStream(path + "\\" + filename);
+		workbook.write(fos);
+		
+		fos.close();
+		workbook.close();
+		
+		}
+	
+	public static void main(String[] args) {
+		String path = "C:\\Users\\Soojung Do\\Desktop\\programmingclass";
+		String filename = "writetest.xlsx";
+		
+		ExcelMain em = new ExcelMain();
+		
+		try {
+//			em.read(path, filename);
+			em.write(path, filename);
+		} catch(IOException e) {
+			e.printStackTrace();
+			System.out.println("에러 발생, 파일 생성이 불가능합니다.");
+		}
+	}
+}
