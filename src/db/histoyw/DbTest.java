e011024562603f167d193c962a85e2e8da938f01
dagki <dagki@LAPTOP-KGM0BO9L>
2021-09-06 09:19:10 +0900

    db

---
+++ src/db/histoyw/DbTest.java
@@ -0,0 +1,21 @@
+package db.histoyw;
+
+public class DbTest {
+	
+	public String select() {
+		return "";
+	}
+	
+	public int insert(String name, String phone, String addr) {
+		return 0;
+	}
+	
+	public int delete(String condition) {
+		return 0;
+	}
+	
+	public int update(String condition) {
+		return 0;
+	}
+
+}
