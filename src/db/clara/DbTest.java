6ca1678111b2b6ff3d4c2d522414643f43db3ee8
tiger <tiger@DESKTOP-EQ1L9CU>
2021-09-06 09:19:50 +0900

    db

--- src/db/clara/DbTest.java
+++ src/db/clara/DbTest.java
@@ -1,5 +1,28 @@
 package db.clara;
 
 public class DbTest {
+	
+	public String select() {
+		
+		return "";
+	}
+	
+	public int insert(String name, String phone, String addr) {  //int눈 건수로 확인
+		
+		return 0;
+	
+	}
+	
+	public int delete(String condition) {
+		
+		return 0;
+		
+	}
+	
+	public int update(String condition) {
 
+		return 0;
+		
+	}
+	
 }
