5e3d5c898fd28b4d9d338f2210dc5d10e4fd0e37
USER <USER@DESKTOP-GMDH2V2>
2021-09-06 09:23:53 +0900

    seravision

---
+++ src/db/seravision/DBTest.java
@@ -0,0 +1,28 @@
+package db.seravision;
+
+public class DBTest {
+
+	 public String select (){
+		 
+		 return "";
+		 
+	 }
+	 
+	 public int insert (String name, String phone, String addr){
+		 
+		 return 0;
+	 }
+
+	 public int  delete (String condition){
+		 return 0;
+	 
+	 }
+
+	 public int  update (String condition){
+		 return 0;
+		 
+	 }
+
+	
+	 
+}
