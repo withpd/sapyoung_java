6709fe042e098066255e1e02ac2640927603945a
withpd <withpd@gmail.com>
2021-09-08 16:54:29 +0900

    SELECT Query

--- src/db/withpd/DbTest.java
+++ src/db/withpd/DbTest.java
@@ -3,29 +3,53 @@ package db.withpd;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.sapyoung.Constants;
 
 public class DbTest {
 	
-	public int delete(String condition) {
-		// 접속정보
-		String host = "database-1.cjvdgquniwjw.ap-northeast-2.rds.amazonaws.com";
-		String port = "3306";
-		String sid = "geek9";
-		String id = "admin";
-		String pw = "1234";
-		String driver = "com.mysql.cj.jdbc.Driver";
+	// 접속정보
+	String host = "database-1.cjvdgquniwjw.ap-northeast-2.rds.amazonaws.com";
+	String port = "3306";
+	String sid;
+	String id;
+	String pw;
+	String driver = "com.mysql.cj.jdbc.Driver";
+	
+	Connection con = null;
+	
+	public DbTest() {
 		
-		// 실행할 쿼리(DELETE)
-		String query = "DELETE FROM geek9.Test WHERE name = ?;";
+	}
+
+	public DbTest(String id, String pw, String sid) {
+		this.id = id;
+		this.pw = pw;
+		this.sid = sid;
+	}
+	
+	public void connect() {
 		
-		int result = 0;
 		try {
 			Class.forName(driver);
 			
 			// 1. DB 접속
 			String url = "jdbc:mysql://" + host + ":" + port + "/" + sid;
 			// jdbc:mysql://database-1.cjvdgquniwjw.ap-northeast-2.rds.amazonaws.com:3306/geek9
-			Connection con = DriverManager.getConnection(url, id, pw);
+			con = DriverManager.getConnection(url, id, pw);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public int delete(String condition) {
+		
+		// 실행할 쿼리(DELETE)
+		String query = "DELETE FROM geek9.Test WHERE name = ?;";
+		
+		int result = 0;
+		try {
 			
 			// 2. Delete Query 실행
 			PreparedStatement pstmt = con.prepareStatement(query);
@@ -44,26 +68,13 @@ public class DbTest {
 	
 	public int insert(String name, String phone, String addr) {
 		
-		// 접속정보
-		String host = "database-1.cjvdgquniwjw.ap-northeast-2.rds.amazonaws.com";
-		String port = "3306";
-		String sid = "geek9";
-		String id = "admin";
-		String pw = "1234";
-		String driver = "com.mysql.cj.jdbc.Driver";
-		
 		// 실행할 쿼리(INSERT)
 		String query = "INSERT INTO geek9.Test(name, phone, addr) VALUES(?, ?, ?)";
 //		String query = "INSERT INTO geek9.Test(name, phone, addr) VALUES('" + name + "', '" + phone + "', '" + addr + "')";
 		// 			   "INSERT INTO geek9.Test(name, phone, addr) VALUES('    배성호    ',     '1111    ', '     인천     ')"
 		int result = 0;
 		try {
-			Class.forName(driver);
-			
-			// 1. DB 접속
-			String url = "jdbc:mysql://" + host + ":" + port + "/" + sid;
-			// jdbc:mysql://database-1.cjvdgquniwjw.ap-northeast-2.rds.amazonaws.com:3306/geek9
-			Connection con = DriverManager.getConnection(url, id, pw);
+		
 			
 			// 2. Insert Query 실행
 			PreparedStatement pstmt = con.prepareStatement(query);
@@ -82,32 +93,49 @@ public class DbTest {
 		return result;
 	}
 	
-	public String select() {
+	public String select(String condition) {
+	
+		condition = "%" + condition + "%";
+		
+		// 실행할 쿼리(SELECT)
+		String query = "SELECT * FROM geek9.Test WHERE phone like ?" ;
+		
+		ResultSet rs = null;
+		try {
+		
+			
+			// 2. SELECT Query 실행
+			PreparedStatement pstmt = con.prepareStatement(query);
+			pstmt.setString(1, condition);
+			
+			rs = pstmt.executeQuery();	// select
+			
+			while(rs.next()) {
+				String name = rs.getString(1);
+				String phone = rs.getString(2);
+				String location = rs.getString(3);
+				
+				System.out.println(name);
+				System.out.println(phone);
+				System.out.println(location);
+			}
+			
+			// 3. 결과 확인
+			System.out.println("결과 : " + rs);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return "";
 	}
 	
-
-	
 	public int update(String oldVal, String newVal) {
-		// 접속정보
-		String host = "database-1.cjvdgquniwjw.ap-northeast-2.rds.amazonaws.com";
-		String port = "3306";
-		String sid = "geek9";
-		String id = "admin";
-		String pw = "1234";
-		String driver = "com.mysql.cj.jdbc.Driver";
 		
 		// 실행할 쿼리(UPDATE)
 		String query = "UPDATE geek9.Test SET name=? where addr = ?;";
 		
 		int result = 0;
 		try {
-			Class.forName(driver);
-			
-			// 1. DB 접속
-			String url = "jdbc:mysql://" + host + ":" + port + "/" + sid;
-			// jdbc:mysql://database-1.cjvdgquniwjw.ap-northeast-2.rds.amazonaws.com:3306/geek9
-			Connection con = DriverManager.getConnection(url, id, pw);
 			
 			// 2. UPDATE Query 실행
 			PreparedStatement pstmt = con.prepareStatement(query);
@@ -116,7 +144,7 @@ public class DbTest {
 			result = pstmt.executeUpdate();
 			
 			// 3. 결과 확인
-			System.out.println("결과 : " + result);
+//			System.out.println("결과 : " + result);
 			
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -125,10 +153,12 @@ public class DbTest {
 	}
 	
 	public static void main(String[] arg) {
-		DbTest db = new DbTest();
-		// db.insert("영수", "100000", "대전");	// 이름/폰/주소
+		DbTest db = new DbTest("admin", "1234", "geek9");
+		db.connect();		// 접속	
+//		db.insert("영수", "100000", "대전");	// 이름/폰/주소
 //		db.delete("배성호");
-		db.update("인천", "배성호");
+//		db.update("인천", "배성호");
+		db.select("8235");
 		
 	}
 }
